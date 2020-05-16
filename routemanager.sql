0d3d5153fa0501bcfa9dd2641b658374be651f1f
Boris Predmerski <Boris Predmerskcls>
2020-05-17 00:05:59 +0200

    Update sql skripte sa tapbilocm prema er dijagramu

--- routemanager.sql
+++ routemanager.sql
@@ -11,3 +11,20 @@ mobitel int not null,
 email varchar (50) not null
 );
 
+create table vozac (
+id int not null primary key auto_increment,
+osobaid int not null,
+brvozacke varchar (50)
+
+);
+
+create table klijent (
+id int not null primary key auto_increment,
+osobaid int not null,
+turnus int not null,
+adresaid int not null
+
+);
+
+
+
